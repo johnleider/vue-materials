fe9d0e2f898f8d877e85865f85fc36b0a2e2efe9
John Leider <john.j.leider@gmail.com>
2016-07-07 22:23:27 -0400

    mixin for text/text-area counter

---
+++ src/mixins/counter.js
@@ -0,0 +1,14 @@
+import Load from '../util/load'
+
+export default {
+    props: {
+        length: {
+            type: [Number, String, Boolean],
+            default: false
+        }
+    },
+
+    mounted () {
+        Load.call(this, () => $(this.$el).characterCounter())
+    }
+}
