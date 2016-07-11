51e93049696acaedf8afc3651c45c38bf380d8d1
John Leider <john.j.leider@gmail.com>
2016-07-11 18:44:39 -0400

    added fixed property

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,9 +1,22 @@
 <template>
-    <nav>
-        <div class="nav-wrapper">
-            <slot name="logo"></slot>
-            <slot name="items"></slot>
-            <slot name="side-nav"></slot>
-        </div>
-    </nav>
+    <div :class="{ 'navbar-fixed': fixed }">
+        <nav>
+            <div class="nav-wrapper">
+                <slot name="logo"></slot>
+                <slot></slot>
+                <slot name="side-nav"></slot>
+            </div>
+        </nav>
+    </div>
 </template>
+
+<script type="text/babel">
+    export default {
+        props: {
+            fixed: {
+                type: Boolean,
+                default: false
+            }
+        }
+    }
+</script>
