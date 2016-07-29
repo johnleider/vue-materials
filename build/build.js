80e0c964eb2506fd3de24fb572ccab411d43ee48
John Leider <john.j.leider@gmail.com>
2016-07-28 20:26:33 -0400

    removed deprecated file

--- build/build.js
+++
@@ -1,9 +0,0 @@
-var rollup = require('rollup')
-var babel = require('rollup-plugin-babel')
-
-rollup.rollup({
-    entry: '../src/index.js',
-    format: 'cjs',
-    out: 'dist/vue-materials.common.js',
-    plugins: [babel()]
-})
