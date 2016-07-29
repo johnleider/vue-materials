4275e05a3361dbd7026c7dd6936e6ae11255c85f
John Leider <john.j.leider@gmail.com>
2016-07-28 20:25:27 -0400

    removed click events as 2.0 now supports .native events

--- src/components/button-link.vue
+++ src/components/button-link.vue
@@ -8,9 +8,6 @@
        :data-gutter="gutter"
        :data-beloworigin="beloworigin"
        :data-alignment="alignment"
-       @click="$emit('click')"
-       @mouseenter="$emit('mouseenter')"
-       @mouseleave="$emit('mouseleave')"
     >
         <slot></slot>
     </a>
