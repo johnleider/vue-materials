252daa0e1f9c0f000a5f1c60abf3b5bb29a8015b
John Leider <john.j.leider@gmail.com>
2017-03-08 21:15:56 -0500

    fix for #53

--- src/directives/modal.js
+++ src/directives/modal.js
@@ -3,13 +3,14 @@ import Load from '../util/load'
 export default {
     bind (el, binding, vnode) {
         Load.call(vnode.context, () => {
+            const params = binding.value || {}
             if (el.nodeName === 'button') {
-                el.setAttribute('data-target', binding.arg)
+                el.setAttribute('data-target', binding.arg || params.value)
             } else {
-                el.setAttribute('href', `#${binding.arg}`)
+                el.setAttribute('href', `#${binding.arg || params.value}`)
             }
             
-            $(`#${binding.arg}`).modal(binding.value || {})
+            $(`#${binding.arg || params.value}`).modal()
         })
     }
 }
